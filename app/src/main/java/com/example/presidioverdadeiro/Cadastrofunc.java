ff24d29de8b449d72d7dcd318f12a96183ab594f
antonio naranjo <totyjnc@gmail.com>
2021-11-24 18:57:32 -0300

    primeira versão com cadastro e mainlogin

---
+++ app/src/main/java/com/example/presidioverdadeiro/Cadastrofunc.java
@@ -0,0 +1,54 @@
+package com.example.presidioverdadeiro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class Cadastrofunc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrofunc);
+
+        TextView Usuárioc = (TextView) findViewById(R.id.Usuárioc);
+        TextView passwordc = (TextView) findViewById(R.id.passwordc);
+        TextView passwordconfirm = (TextView) findViewById(R.id.passwordconfirm);
+        TextView Email = (TextView) findViewById(R.id.Email);
+        TextView idpolicial = (TextView) findViewById(R.id.idpolicial);
+
+        MaterialButton btncadastro= (MaterialButton) findViewById(R.id.btncadastro);
+
+        //admin e admin
+
+        btncadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Usuárioc.getText().toString().equals("") || passwordc.getText().equals("") || passwordconfirm.getText().equals("") || Email.getText().toString().equals("") ||  idpolicial.getText().toString().equals("") )
+                {
+                    Toast.makeText(Cadastrofunc.this, "Porfavor Preencha todos as caixas de texto!!!",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if(passwordc.getText().toString().equals(passwordconfirm.getText().toString()) ){
+                        Toast.makeText(Cadastrofunc.this, "Cadastro realizado com sucesso!",Toast.LENGTH_SHORT).show();
+                        openHomescreen();
+                    }
+                    else if (passwordc.getText().toString() != passwordconfirm.getText().toString()){
+                        Toast.makeText(Cadastrofunc.this, "As senhas precisam ser identicas!!!",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+    }
+    public void openHomescreen() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/presidioverdadeiro/Tela_principal.java
@@ -0,0 +1,90 @@
+package com.example.presidioverdadeiro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class Tela_principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+
+
+
+        MaterialButton adicionarbtn = (MaterialButton) findViewById(R.id.adicionarbtn);
+        MaterialButton cadastrarbtn = (MaterialButton) findViewById(R.id.cadastrarbtn);
+        MaterialButton consultarbtn = (MaterialButton) findViewById(R.id.consultarbtn);
+        MaterialButton logoutbtn = (MaterialButton) findViewById(R.id.logoutbtn);
+
+
+
+        adicionarbtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Toast.makeText(Tela_principal.this, "Redirecionando", Toast.LENGTH_SHORT).show();
+
+
+            }
+
+
+        });
+
+
+        cadastrarbtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Toast.makeText(Tela_principal.this, "Redirecionando", Toast.LENGTH_SHORT).show();
+
+
+            }
+
+
+        });
+
+
+        consultarbtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Toast.makeText(Tela_principal.this, "Redirecionando", Toast.LENGTH_SHORT).show();
+
+
+            }
+
+
+        });
+
+        logoutbtn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Toast.makeText(Tela_principal.this, "Saindo...", Toast.LENGTH_SHORT).show();
+                OpenLoginScreen();
+
+
+            }
+
+
+        });
+
+
+
+
+    }
+
+    public void OpenLoginScreen() {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+
+    }
