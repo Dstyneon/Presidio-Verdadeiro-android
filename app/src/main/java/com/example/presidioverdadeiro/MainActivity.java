798eed68d3abc6963a2bfd55200c2e3703aa9e03
Daniel Galdencio <daniel.galdencio@aluno.ifsp.edu.br>
2021-11-28 23:57:44 -0300

    Sincronização da tela de login para logar apenas com usuários já cadastrados (CORREÇÃO)

---
+++ app/src/main/java/com/example/presidioverdadeiro/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.presidioverdadeiro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    DBHelper DB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView id = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.lognbtn);
+        MaterialButton Cadastrar = (MaterialButton) findViewById(R.id.cadastrar);
+
+        DB = new DBHelper(this);
+
+        //admin e admin
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (id.getText().toString().equals("") || password.getText().toString().equals("") ){ //se o campo id ou o campo senha estiverem em branco
+
+                    Toast.makeText(MainActivity.this, "Por favor, preencha todos os campos",Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+
+                    String id_policial = id.getText().toString();
+                    String senha = password.getText().toString();
+
+                    Boolean checksenha = DB.checksenha(id_policial, senha);
+
+                    if (checksenha == true) { //se o usuario e/ou a senha existirem
+                        Toast.makeText(MainActivity.this, "Bem vindo(a)", Toast.LENGTH_SHORT).show();
+                        AbrirHomeScreen();
+                    }
+                    else { //se o usuario e/ou a senha não existirem
+                        Toast.makeText(MainActivity.this, "Credenciais inválidas, tente novamente", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+
+            }
+
+
+
+        });
+
+        Cadastrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View v){
+                AbrirTelaCadastro();
+            }
+        });
+
+
+    }
+
+
+
+
+
+    public void AbrirHomeScreen() {
+        Intent intent = new Intent(this,Tela_principal.class);
+        startActivity(intent);
+    }
+    public void AbrirTelaCadastro() {
+        Intent intent = new Intent(this,Cadastrofunc.class);
+        startActivity(intent);
+    }
+
+
+}
